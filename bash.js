b37ff43641d41d686a78e3ba15d1c64f3a768d38
Rajiv Bhatia <rajivbhatia@outlook.com>
2018-07-31 11:43:41 -0500

    last switch

--- bash.js
+++ bash.js
@@ -11,6 +11,7 @@
 
 const pwd = require('./pwd.js')
 const ls = require('./ls.js')
+const cat = require('./cat.js')
 
 process.stdout.write('prompt > ');
 
@@ -21,7 +22,12 @@ process.stdin.on('data', (data) => {
     pwd()
   } else if (cmd == 'ls'){
     ls()
-  } else {
+  } else if (cmd.slice(0,4) = "cat ") {
+    let cmd2 = cmd.slice(4, cmd.length);
+    cat(cmd2);
+  }
+
+  else {
   process.stdout.write('You typed: ' + cmd);
   }
   process.stdout.write('\nprompt > ');
---
+++ cat.js
@@ -0,0 +1,11 @@
+// const fs = require('fs')
+
+module.exports = function() {
+  // fs.readFile(filePath, (err,data) => {
+  //   if (err) throw err;
+  //   process.stdout.write(data);
+  // }
+  process.stdout.write("hello");
+}
+
+
